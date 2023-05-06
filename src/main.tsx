8aaede81b2e8c202082a59bdbcf7f20fc55b38e3
himanshu <hmxnsu.blvck@gmail.com>
2023-05-07 02:17:31 +0530

    ....

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import AppRouter from './routes'
-import dotenv from 'dotenv'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor, store } from './redux/Store'
+// import dotenv from 'dotenv'
 
-dotenv.config()
+// dotenv.config()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <AppRouter />
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <AppRouter />
+        </PersistGate>
+    </Provider>
 )
